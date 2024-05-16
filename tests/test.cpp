ecbaf0f612dd582446aa9c491421b356e3b7affc
ChushkinMikhail <michael.tchushkin@yandex.ru>
2024-05-16 03:42:34 +0300

    Upd6

--- tests/test.cpp
+++ tests/test.cpp
@@ -3,24 +3,43 @@
 #include <sstream>
 #include "./../builder.cpp"
 
-TEST(LOLGameDeviceTest, SetMouseTest) {
-  jc::LOLGameDevice LOLBuilder;
-  LOLBuilder.BuildMouse();
-  EXPECT_EQ(LOLBuilder.Device().SetMouse("Logitech"), "Logitech");
+TEST(GameDeviceTest, LOLBuilderTest) {
+  jc::LOLGameDevice builder;
+  builder.BuildMouse();
+  builder.BuildKeyboard();
+  builder.BuildHeadphone();
+
+  jc::DeviceSuite device = builder.Device();
+
+  EXPECT_EQ(device.mouse, "Logitech");
+  EXPECT_EQ(device.keyboard, "Filco");
+  EXPECT_EQ(device.headphone, "Sennheiser");
 }
 
-TEST(DNFGameDeviceTest, SetKeyboardTest) {
-  jc::DNFGameDevice DNFBuilder;
-  DNFBuilder.BuildKeyboard();
-  EXPECT_EQ(DNFBuilder.Device().SetKeyboard("Cherry"), "Cherry");
+TEST(GameDeviceTest, DNFBuilderTest) {
+  jc::DNFGameDevice builder;
+  builder.BuildMouse();
+  builder.BuildKeyboard();
+  builder.BuildHeadphone();
+
+  jc::DeviceSuite device = builder.Device();
+
+  EXPECT_EQ(device.mouse, "Razer");
+  EXPECT_EQ(device.keyboard, "Cherry");
+  EXPECT_EQ(device.headphone, "Beyerdynamic");
 }
 
 TEST(LifeTest, CreateDeviceTest) {
   jc::Life life;
   jc::LOLGameDevice LOLBuilder;
   jc::DeviceSuite LOLDevice = life.CreateDevice(LOLBuilder);
-  
-  EXPECT_EQ(LOLDevice.SetMouse("Logitech"), "Logitech");
-  EXPECT_EQ(LOLDevice.SetKeyboard("Filco"), "Filco");
-  EXPECT_EQ(LOLDevice.SetHeadphone("Sennheiser"), "Sennheiser");
+
+  EXPECT_EQ(LOLDevice.mouse, "Logitech");
+  EXPECT_EQ(LOLDevice.keyboard, "Filco");
+  EXPECT_EQ(LOLDevice.headphone, "Sennheiser");
+}
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
