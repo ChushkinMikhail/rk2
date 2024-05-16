c6b09b739d875f4db0a1036634b24fd9ab56b5a5
ChushkinMikhail <michael.tchushkin@yandex.ru>
2024-05-16 03:57:15 +0300

    Upd6

--- tests/test.cpp
+++ tests/test.cpp
@@ -3,43 +3,95 @@
 #include <sstream>
 #include "./../builder.cpp"
 
-TEST(GameDeviceTest, LOLBuilderTest) {
-  jc::LOLGameDevice builder;
-  builder.BuildMouse();
-  builder.BuildKeyboard();
-  builder.BuildHeadphone();
+namespace jc {
 
-  jc::DeviceSuite device = builder.Device();
+class DeviceSuite {
+ public:
+  void SetMouse(std::string_view) {}
+  void SetKeyboard(std::string_view) {}
+  void SetHeadphone(std::string_view) {}
+};
 
-  EXPECT_EQ(device.mouse, "Logitech");
-  EXPECT_EQ(device.keyboard, "Filco");
-  EXPECT_EQ(device.headphone, "Sennheiser");
-}
+class GameDevice {
+ public:
+  virtual void BuildMouse() = 0;
+  virtual void BuildKeyboard() = 0;
+  virtual void BuildHeadphone() = 0;
+  virtual DeviceSuite& Device() const = 0;
+  virtual ~GameDevice() = default;
+};
 
-TEST(GameDeviceTest, DNFBuilderTest) {
-  jc::DNFGameDevice builder;
-  builder.BuildMouse();
-  builder.BuildKeyboard();
-  builder.BuildHeadphone();
+class LOLGameDevice : public GameDevice {
+ public:
+  LOLGameDevice() : device_(std::make_unique<DeviceSuite>()) {}
 
-  jc::DeviceSuite device = builder.Device();
+  void BuildMouse() override { device_->SetMouse("Logitech"); }
 
-  EXPECT_EQ(device.mouse, "Razer");
-  EXPECT_EQ(device.keyboard, "Cherry");
-  EXPECT_EQ(device.headphone, "Beyerdynamic");
-}
+  void BuildKeyboard() override { device_->SetKeyboard("Filco"); }
 
-TEST(LifeTest, CreateDeviceTest) {
+  void BuildHeadphone() override { device_->SetHeadphone("Sennheiser"); }
+
+  DeviceSuite& Device() const override { return *device_; }
+
+ private:
+  std::unique_ptr<DeviceSuite> device_;
+};
+
+class DNFGameDevice : public GameDevice {
+ public:
+  DNFGameDevice() : device_(std::make_unique<DeviceSuite>()) {}
+
+  void BuildMouse() override { device_->SetMouse("Razer"); }
+
+  void BuildKeyboard() override { device_->SetKeyboard("Cherry"); }
+
+  void BuildHeadphone() override { device_->SetHeadphone("Beyerdynamic"); }
+
+  DeviceSuite& Device() const override { return *device_; }
+
+ private:
+  std::unique_ptr<DeviceSuite> device_;
+};
+
+class Life {
+ public:
+  DeviceSuite& CreateDevice(GameDevice& builder) {
+    builder.BuildMouse();
+    builder.BuildKeyboard();
+    builder.BuildHeadphone();
+    return builder.Device();
+  }
+};
+
+} 
+
+void testLOLGameDevice() {
   jc::Life life;
   jc::LOLGameDevice LOLBuilder;
-  jc::DeviceSuite LOLDevice = life.CreateDevice(LOLBuilder);
 
-  EXPECT_EQ(LOLDevice.mouse, "Logitech");
-  EXPECT_EQ(LOLDevice.keyboard, "Filco");
-  EXPECT_EQ(LOLDevice.headphone, "Sennheiser");
+  jc::DeviceSuite LOLDevice = life.CreateDevice(LOLBuilder);
 }
 
-int main(int argc, char **argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+void testDNFGameDevice() {
+  jc::Life life;
+  jc::DNFGameDevice DNFBuilder;
+
+  jc::DeviceSuite DNFDevice = life.CreateDevice(DNFBuilder);
+}
+
+void testLife() {
+  jc::Life life;
+  jc::LOLGameDevice LOLBuilder;
+  jc::DNFGameDevice DNFBuilder;
+
+  jc::DeviceSuite LOLDevice = life.CreateDevice(LOLBuilder);
+  jc::DeviceSuite DNFDevice = life.CreateDevice(DNFBuilder);
+}
+
+int main() {
+  testLOLGameDevice();
+  testDNFGameDevice();
+  testLife();
+
+  return 0;
 }
