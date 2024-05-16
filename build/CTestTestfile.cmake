a4c5ae03e7cfbe864cb5eac5acda7a4c5cbd3e79
ChushkinMikhail <michael.tchushkin@yandex.ru>
2024-05-16 16:53:30 +0300

    Sorry very last commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/vboxuser/ChushkinMikhail/workspace/rk2
+# Build directory: /home/vboxuser/ChushkinMikhail/workspace/rk2/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(check "/home/vboxuser/ChushkinMikhail/workspace/rk2/build/check")
+set_tests_properties(check PROPERTIES  _BACKTRACE_TRIPLES "/home/vboxuser/ChushkinMikhail/workspace/rk2/CMakeLists.txt;13;add_test;/home/vboxuser/ChushkinMikhail/workspace/rk2/CMakeLists.txt;0;")
+subdirs("third-party/gtest")
